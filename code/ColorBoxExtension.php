da3d2604cbe1c79292a41e196d0f9985aed75df4
helpfulrobot <chris@silverstripe.com>
2016-01-01 03:05:07 +1300

    Converted to PSR-2

--- code/ColorBoxExtension.php
+++ code/ColorBoxExtension.php
@@ -1,15 +1,14 @@
 <?php
 
-class ColorBoxExtension extends Extension{
-	
-	function contentcontrollerInit(){
-		
-		Requirements::themedCSS("colorbox");
-		
-		Requirements::javascript(THIRDPARTY_DIR.'/jquery/jquery.js');
-		Requirements::javascript("colorbox/javascript/jquery.colorbox-min.js");
-		Requirements::javascript("colorbox/javascript/colorbox.js");
-		
-	}
-	
+class ColorBoxExtension extends Extension
+{
+    
+    public function contentcontrollerInit()
+    {
+        Requirements::themedCSS("colorbox");
+        
+        Requirements::javascript(THIRDPARTY_DIR.'/jquery/jquery.js');
+        Requirements::javascript("colorbox/javascript/jquery.colorbox-min.js");
+        Requirements::javascript("colorbox/javascript/colorbox.js");
+    }
 }
